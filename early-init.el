2375aa3546538081e4115c63a9266a63ceae466b
sz <698307+xparq@users.noreply.github.com>
2023-08-27 22:56:09 +0200

    Forced (protected) keymap created in keys.el
    + More examples in early-init.el

--- early-init.el
+++ early-init.el
@@ -1,3 +1,7 @@
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Init-File.html
+;; Prevent site-lisp init (but there's also a command line option for that!)
+;;(setq site-run-file nil)
+
 ;; Package init is done automatically, before `user-init-file' is loaded,
 ;; but after `early-init-file'. If we handle package initialization manually,
 ;; we must prevent Emacs from doing it early!
@@ -6,3 +10,4 @@
 ;; `use-package' is builtin since 29.
 ;; This can be set here, before loading `use-package':
 ;(setq use-package-enable-imenu-support t)
+
--- sz/load/my/keys.el
+++ sz/load/my/keys.el
@@ -12,6 +12,53 @@
 ;;   
 ;;
 
+
+;;-----------------------------------------------------------------------------
+;; force-map-quit-key.el <- should be in my/lib.el, "functionified"?
+;;-----------------------------------------------------------------------------
+;;!!??
+;;!!?? How to make this a function, with all the autoload stuff?
+;;!!??
+;;!!?? (defun sz/setup-forced-keymap ; -> https://github.com/xparq/.emacs.d/issues/7
+;;!!??    "Facilitate defining key bindings with high(est) priority" ; https://github.com/kaushalmodi/.emacs.d/blob/master/elisp/modi-mode.el
+
+  (defvar sz/forced-keymap (make-sparse-keymap)
+    "High-precedence keymap for 'sticky' bindings.")
+
+;;;###autoload
+  (define-minor-mode sz/forced-keymap-mode
+    "A minor mode for key bindings to override major (and most minor) modes."
+    ;; If init-value is not set to t, this mode does not get enabled in
+    ;; `fundamental-mode' buffers even after doing \"(sz/global-forced-keymap-mode 1)\".
+    ;; More info: http://emacs.stackexchange.com/q/16693/115
+    :init-value t
+    :lighter " sz/forced-keymap-mode"
+    :keymap sz/forced-keymap)
+
+;;;###autoload
+  (define-globalized-minor-mode sz/global-forced-keymap-mode
+    sz/forced-keymap-mode
+    sz/forced-keymap-mode)
+
+  ;; https://github.com/jwiegley/use-package/blob/master/bind-key.el
+  ;; The keymaps in `emulation-mode-map-alists' take precedence over
+  ;; `minor-mode-map-alist'
+  (add-to-list 'emulation-mode-map-alists `((sz/forced-keymap-mode . ,sz/forced-keymap)))
+
+  ;; Turn off the minor mode in the minibuffer
+  (defun turn-off-my-mode ()
+    "Turn off sz/forced-keymap-mode."
+    (sz/forced-keymap-mode -1))
+  (add-hook 'minibuffer-setup-hook #'turn-off-my-mode)
+
+  (provide 'sz/forced-keymap-mode)
+
+;;!!??
+;;!!?? );; Minor mode tutorial: http://nullprogram.com/blog/2013/02/06/
+;;!!??
+;;-----------------------------------------------------------------------------
+
+
 ;; Tab...
 ;;(setq-default indent-tabs-mode t)
 ;;(setq delete-trailing-whitespace nil) ;; Prevent Backspace to delete everything that's "white", instead of just the last char...
